1b659716b0454465f4fbfa7bfa5e5e21dba8439a
A-shir1 <ma18u10329@btech.nitdgp.ac.in>
2021-10-18 12:19:31 +0530

    lots of changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let textBox = document.querySelectorAll(".textbox");
+let errorMsg = document.querySelectorAll(".errorMsg");
 let percButt = document.querySelectorAll("input[name='percentage']");
 let percText = document.querySelector("#percText");
 let tipNum = document.querySelector(".tipNum");
@@ -7,6 +8,8 @@ let calcButt = document.querySelector(".calcButt");
 let resetButt = document.querySelector(".resetButt");
 
 let bill = 0, nop = 0, percentage = 0, totalAmountPerP = 0, tipAmountPerP = 0, total = 0, tipTotal = 0;
+
+
 
 
         // CALCULATE BUTTON FUNCTIONS
@@ -22,6 +25,7 @@ const update = (tipAmountPerP, totalAmountPerP) =>{
     tipNum.innerText = `${tipAmountPerP.toFixed(2)}`;
     totalNum.innerHTML = `${totalAmountPerP.toFixed(2)}`;
 }
+
 
         // RESET BUTTON FUNCTIONS   
 
@@ -51,16 +55,20 @@ const resetPage = () => {
     totalNum.innerHTML = '0.00';
 }
 
-const checkInputs = () => {
-    if (bill > 0 && nop > 0 && percentage > 0){
-        calcButt.disabled = false;
-        resetButt.disabled = false;
-    }
+const addRedBorder = (tb, index) => {
+    tb.classList.add("tbError");
+    errorMsg[index].classList.remove("hide");
+}
+
+const removeRedBorder = (tb, index) => {
+    tb.classList.remove("tbError");
+    errorMsg[index].classList.add("hide");
 }
 
 
 for (let percValue of percButt) {
     percValue.onchange = function() {
+        percText.value = '';
         percentage = parseFloat(percValue.value);
     }
 }
@@ -78,6 +86,17 @@ for (let i = 0; i < textBox.length; i++){
         else if (i === 1)
             nop = parseFloat(textBox[i].value);
     });
+
+    textBox[i].onclick = () => {
+        textBox[i].select();
+    }
+
+    textBox[i].addEventListener("focusout", () => {
+        if (textBox[i].value === '')
+            addRedBorder(textBox[i], i);
+        else
+            removeRedBorder(textBox[i], i);
+    });
 }
 
 calcButt.addEventListener("click", () => {
@@ -90,4 +109,6 @@ resetButt.addEventListener("click", () => {
     resetRadioButt();
     resetValue();
     resetPage();
+    for (let i = 0; i < textBox.length; i++)
+        removeRedBorder(textBox[i], i);
 });
